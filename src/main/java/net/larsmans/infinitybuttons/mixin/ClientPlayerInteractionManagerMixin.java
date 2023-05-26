69e323a55f141e90abe62de86e98e0447a6b383c
LarsMans <l.s.vanemmerik@gmail.com>
2023-05-27 01:02:36 +0200

    fix full hand on safety button

---
+++ src/main/java/net/larsmans/infinitybuttons/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,35 @@
+package net.larsmans.infinitybuttons.mixin;
+
+import net.larsmans.infinitybuttons.block.custom.emergencybutton.SafeEmergencyButton;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public abstract class ClientPlayerInteractionManagerMixin {
+
+    @Shadow @Final private ClientPlayNetworkHandler networkHandler;
+
+    @Inject(method = "interactBlock", at = @At("HEAD"), cancellable = true)
+    private void interactBlock(ClientPlayerEntity player, ClientWorld world, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        BlockState state = world.getBlockState(hitResult.getBlockPos());
+        Block block = state.getBlock();
+        if (block instanceof SafeEmergencyButton) {
+            this.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, hitResult));
+            cir.setReturnValue(block.onUse(state, world, hitResult.getBlockPos(), player, hand, hitResult));
+        }
+    }
+}
