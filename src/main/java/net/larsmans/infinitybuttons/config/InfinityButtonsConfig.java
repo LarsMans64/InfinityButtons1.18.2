262f857dcca41f935a5b32a177fa627f000604d2
LarsMans <l.s.vanemmerik@gmail.com>
2023-05-26 22:43:24 +0200

    language fixes

--- src/main/java/net/larsmans/infinitybuttons/config/InfinityButtonsConfig.java
+++ src/main/java/net/larsmans/infinitybuttons/config/InfinityButtonsConfig.java
@@ -7,7 +7,7 @@ import me.shedaniel.autoconfig.annotation.ConfigEntry;
 @Config(name = "infinitybuttons")
 @Config.Gui.Background("infinitybuttons:textures/block/copper_button.png")
 public class InfinityButtonsConfig implements ConfigData {
-    @ConfigEntry.Gui.Tooltip
+    @ConfigEntry.Gui.Tooltip(count = 3)
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
     public AlarmEnum alarmSoundType = AlarmEnum.RANGE;
 
