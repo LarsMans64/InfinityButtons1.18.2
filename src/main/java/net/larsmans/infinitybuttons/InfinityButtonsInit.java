0d841b914ebec8073dde146166b10f1d2398f1fc
LarsMans <l.s.vanemmerik@gmail.com>
2023-06-17 18:31:07 +0200

    Letter Button part 1

--- src/main/java/net/larsmans/infinitybuttons/InfinityButtonsInit.java
+++ src/main/java/net/larsmans/infinitybuttons/InfinityButtonsInit.java
@@ -3,14 +3,21 @@ package net.larsmans.infinitybuttons;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.larsmans.infinitybuttons.block.InfinityButtonsBlocks;
+import net.larsmans.infinitybuttons.block.custom.letterbutton.LetterButton;
+import net.larsmans.infinitybuttons.block.custom.letterbutton.LetterButtonEnum;
+import net.larsmans.infinitybuttons.block.custom.letterbutton.gui.LetterButtonGui;
 import net.larsmans.infinitybuttons.compat.IBCarpenterBlocks;
 import net.larsmans.infinitybuttons.compat.IBNethersDelightBlocks;
 import net.larsmans.infinitybuttons.compat.IBNethersDelightItems;
 import net.larsmans.infinitybuttons.config.InfinityButtonsConfig;
 import net.larsmans.infinitybuttons.item.InfinityButtonsItems;
 import net.larsmans.infinitybuttons.sounds.InfinityButtonsSounds;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +28,14 @@ public class InfinityButtonsInit implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ServerPlayNetworking.registerGlobalReceiver(LetterButtonGui.LETTER_BUTTON_PACKET, (server, player, handler, buf, sender) -> {
+			BlockPos pos = buf.readBlockPos();
+			LetterButtonEnum letterButtonEnum = buf.readEnumConstant(LetterButtonEnum.class);
+			World world = player.getWorld();
+			BlockState state = world.getBlockState(pos);
+			world.setBlockState(pos, state.with(LetterButton.CHARACTER, letterButtonEnum));
+		});
+
 		AutoConfig.register(InfinityButtonsConfig.class, JanksonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(InfinityButtonsConfig.class).getConfig();
 
