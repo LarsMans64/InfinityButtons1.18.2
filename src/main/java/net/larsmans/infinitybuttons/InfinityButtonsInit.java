95fe2438e18c5e15d4a87bfcbf7f26549bec2f9c
curryducker <jur.hoekman@gmail.com>
2023-08-07 23:23:43 +0200

    force jade

--- src/main/java/net/larsmans/infinitybuttons/InfinityButtonsInit.java
+++ src/main/java/net/larsmans/infinitybuttons/InfinityButtonsInit.java
@@ -3,6 +3,7 @@ package net.larsmans.infinitybuttons;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
 import net.larsmans.infinitybuttons.advancement.InfinityButtonsTriggers;
@@ -39,6 +40,8 @@ public class InfinityButtonsInit implements ModInitializer {
 	public static final Identifier LETTER_BUTTON_BLOCK_PACKET = new Identifier(MOD_ID, "letter_button_block");
 	public static final Identifier LETTER_BUTTON_SCREEN_PACKET = new Identifier(MOD_ID, "letter_button_screen");
 	public static final Identifier ALARM_PACKET = new Identifier(MOD_ID, "alarm_packet");
+	public static final Identifier REQUEST_JADE_PACKET = new Identifier(MOD_ID, "request_jade");
+	public static final Identifier JADE_PACKET = new Identifier(MOD_ID, "jade_packet");
 
 	@Override
 	public void onInitialize() {
@@ -76,6 +79,8 @@ public class InfinityButtonsInit implements ModInitializer {
 				world.setBlockState(pos, world.getBlockState(pos).with(LetterButton.CHARACTER, letterButtonEnum));
 			}
 		});
+		ServerPlayNetworking.registerGlobalReceiver(REQUEST_JADE_PACKET,
+				(server, player, handler, buf, sender) -> ServerPlayNetworking.send(player, JADE_PACKET, PacketByteBufs.copy(PacketByteBufs.create().writeBoolean(InfinityButtonsInit.config.forceJadeHiding))));
 	}
 
 	public static void registerDispenserBehavior() {
--- src/main/java/net/larsmans/infinitybuttons/compat/jade/InfinityButtonsPlugin.java
+++ src/main/java/net/larsmans/infinitybuttons/compat/jade/InfinityButtonsPlugin.java
@@ -1,5 +1,6 @@
 package net.larsmans.infinitybuttons.compat.jade;
 
+import net.larsmans.infinitybuttons.InfinityButtonsClientInit;
 import net.larsmans.infinitybuttons.InfinityButtonsInit;
 import net.larsmans.infinitybuttons.block.custom.HoglinMountButton;
 import net.larsmans.infinitybuttons.block.custom.secretbutton.AbstractSecretButton;
@@ -17,6 +18,13 @@ import snownee.jade.api.config.IWailaConfig;
 @WailaPlugin
 public class InfinityButtonsPlugin implements IWailaPlugin {
 
+    private boolean hidden(Identifier config) {
+        if (InfinityButtonsClientInit.getForceHidden())
+            return true;
+        else
+            return IWailaConfig.get().getPlugin().get(config);
+    }
+
     static final Identifier CONFIG_HIDE_SECRET_BUTTONS = new Identifier(InfinityButtonsInit.MOD_ID, "hide_secret_buttons");
     static final Identifier CONFIG_HIDE_TORCH_BUTTONS = new Identifier(InfinityButtonsInit.MOD_ID, "hide_torch_buttons");
 
@@ -26,19 +34,21 @@ public class InfinityButtonsPlugin implements IWailaPlugin {
     public void registerClient(IWailaClientRegistration registration) {
         registration.addConfig(CONFIG_HIDE_SECRET_BUTTONS, true);
         registration.addConfig(CONFIG_HIDE_TORCH_BUTTONS, true);
+        registration.markAsClientFeature(CONFIG_HIDE_SECRET_BUTTONS);
+        registration.markAsClientFeature(CONFIG_HIDE_TORCH_BUTTONS);
         registration.addRayTraceCallback((hitResult, accessor, originalAccessor) -> {
             if (accessor instanceof BlockAccessor blockAccessor) {
-                if (IWailaConfig.get().getPlugin().get(CONFIG_HIDE_SECRET_BUTTONS) && blockAccessor.getBlock() instanceof AbstractSecretButton) {
-                    return registration.blockAccessor().from(blockAccessor).blockState(((AbstractSecretButton) blockAccessor.getBlock()).jadeBlock.getDefaultState()).build();
+                if (hidden(CONFIG_HIDE_SECRET_BUTTONS) && blockAccessor.getBlock() instanceof AbstractSecretButton secretButton) {
+                    return registration.blockAccessor().from(blockAccessor).blockState(secretButton.jadeBlock.getDefaultState()).build();
                 }
-                if (IWailaConfig.get().getPlugin().get(CONFIG_HIDE_SECRET_BUTTONS) && blockAccessor.getBlock() instanceof HoglinMountButton) {
+                if (hidden(CONFIG_HIDE_SECRET_BUTTONS) && blockAccessor.getBlock() instanceof HoglinMountButton) {
                     return registration.blockAccessor().from(blockAccessor).blockState(HOGLIN_MOUNT.getDefaultState()).build();
                 }
-                if (IWailaConfig.get().getPlugin().get(CONFIG_HIDE_TORCH_BUTTONS) && blockAccessor.getBlock() instanceof TorchButton) {
-                    return registration.blockAccessor().from(blockAccessor).blockState(((TorchButton) blockAccessor.getBlock()).jadeBlock.getDefaultState()).build();
+                if (hidden(CONFIG_HIDE_TORCH_BUTTONS) && blockAccessor.getBlock() instanceof TorchButton torchButton) {
+                    return registration.blockAccessor().from(blockAccessor).blockState(torchButton.jadeBlock.getDefaultState()).build();
                 }
-                if (IWailaConfig.get().getPlugin().get(CONFIG_HIDE_TORCH_BUTTONS) && blockAccessor.getBlock() instanceof RedstoneTorchButton) {
-                    return registration.blockAccessor().from(blockAccessor).blockState(((RedstoneTorchButton) blockAccessor.getBlock()).jadeBlock.getDefaultState()).build();
+                if (hidden(CONFIG_HIDE_TORCH_BUTTONS) && blockAccessor.getBlock() instanceof RedstoneTorchButton redstoneTorchButton) {
+                    return registration.blockAccessor().from(blockAccessor).blockState(redstoneTorchButton.jadeBlock.getDefaultState()).build();
                 }
             }
             return accessor;
